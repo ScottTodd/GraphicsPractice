109f0a781efe9fc03fa9131e1d58d1be188134a1
Scott Todd <scott.todd0@gmail.com>
2015-07-27 23:48:51 -0400

    Replaced overloaded Camera constructors with default arguments.

--- src/camera.cc
+++ src/camera.cc
@@ -3,17 +3,6 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-Camera::Camera() {
-    projection = glm::perspective(60.0f, 4.0f / 3.0f, 0.1f, 100.0f);
-    view = glm::lookAt(glm::vec3(0,0,10), glm::vec3(0,0,0), glm::vec3(0,1,0));
-}
-
-Camera::Camera(float fov, float aspect_ratio, float near_clip, float far_clip) {
-    projection = glm::perspective(fov, aspect_ratio, near_clip, far_clip);
-    view = glm::lookAt(glm::vec3(0,0,10), glm::vec3(0,0,0), glm::vec3(0,1,0));
-
-}
-
 Camera::Camera(float fov, float aspect_ratio, float near_clip, float far_clip,
                glm::vec3 position, glm::vec3 target, glm::vec3 up) {
     projection = glm::perspective(fov, aspect_ratio, near_clip, far_clip);
--- src/camera.h
+++ src/camera.h
@@ -6,10 +6,13 @@
 class Camera
 {
 public:
-    Camera();
-    Camera(float fov, float aspect_ratio, float near_clip, float far_clip);
-    Camera(float fov, float aspect_ratio, float near_clip, float far_clip,
-           glm::vec3 position, glm::vec3 target, glm::vec3 up);
+    Camera(float fov = 60.0f,
+           float aspect_ratio = 1.33333f,
+           float near_clip = 0.f,
+           float far_clip = 100.0f,
+           glm::vec3 position = glm::vec3(0,0,10),
+           glm::vec3 target = glm::vec3(0,0,0),
+           glm::vec3 up = glm::vec3(0,1,0));
 
     glm::mat4 TransformModel(glm::mat4 model);
 
