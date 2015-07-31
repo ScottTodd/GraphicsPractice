d01a81751528e8d7731b363e1038455d1a1771ab
Scott Todd <scott.todd0@gmail.com>
2015-07-31 19:29:16 -0400

    Loop to create 10 tetrahedrons.

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 #include <vector>
 
 #include <glm/glm.hpp>
@@ -17,43 +18,49 @@ int main(void) {
     if (!success) { return 0; }
     std::cout << "Scene initialized.\n";
 
-    SceneObject* tetrahedron = new SceneObject();
+    for (int i = 0; i < 10; ++i) {
+        SceneObject* tetrahedron = new SceneObject();
+        tetrahedron->transform.position = glm::vec3(rand() % 8 - 4,
+                                                    rand() % 8 - 4,
+                                                    rand() % 8 - 4);
 
+        Material tetrahedron_mat = Material(
+                "shaders/BasicShading.vertexshader",
+                "shaders/BasicShading.fragmentshader");
 
+        std::vector<float> tetrahedron_vertices = {
+            -1.0f,  0.0f, -0.707f,
+             1.0f,  0.0f, -0.707f,
+             0.0f,  1.0f,  0.707f,
+             0.0f, -1.0f,  0.707f
+        };
+        std::vector<float> tetrahedron_normals = {
+            -0.707f,  0.0f  , -0.707f,
+             0.707f,  0.0f  , -0.707f,
+             0.0f  ,  0.707f, 0.707f,
+             0.0f  , -0.707f, 0.707f
+        };
+        std::vector<unsigned int> tetrahedron_indices = {
+            0, 3, 1,
+            1, 3, 2,
+            2, 3, 0,
+            0, 2, 1
+        };
+        Mesh tetrahedron_mesh = {tetrahedron_vertices,
+                                 tetrahedron_normals,
+                                 tetrahedron_indices};
 
-    Material tetrahedron_mat = Material("shaders/BasicShading.vertexshader",
-                                        "shaders/BasicShading.fragmentshader");
+        MeshRenderer* tetrahedron_renderer = new MeshRenderer(tetrahedron_mesh,
+                                                              tetrahedron_mat);
+        tetrahedron->AddComponent(tetrahedron_renderer);
 
-    std::vector<float> tetrahedron_vertices = {
-        -1.0f,  0.0f, -0.707f,
-         1.0f,  0.0f, -0.707f,
-         0.0f,  1.0f,  0.707f,
-         0.0f, -1.0f,  0.707f
-    };
-    std::vector<float> tetrahedron_normals = {
-        -0.707f,  0.0f  , -0.707f,
-         0.707f,  0.0f  , -0.707f,
-         0.0f  ,  0.707f, 0.707f,
-         0.0f  , -0.707f, 0.707f
-    };
-    std::vector<unsigned int> tetrahedron_indices = {
-        0, 3, 1,
-        1, 3, 2,
-        2, 3, 0,
-        0, 2, 1
-    };
-    Mesh tetrahedron_mesh = {tetrahedron_vertices,
-                             tetrahedron_normals,
-                             tetrahedron_indices};
+        tetrahedron->AddComponent(new Rotate(rand() % 2 - 1.0f,
+                                             rand() % 2 - 1.0f,
+                                             rand() % 2 - 1.0f));
 
-    MeshRenderer* tetrahedron_renderer = new MeshRenderer(tetrahedron_mesh,
-                                                          tetrahedron_mat);
-    tetrahedron->AddComponent(tetrahedron_renderer);
-
-    tetrahedron->AddComponent(new Rotate(0.0f, 0.8f, 0.0f));
-
-    tetrahedron->transform.Translate(0, -1, 0);
-    scene.AddObject(tetrahedron);
+        tetrahedron->transform.Translate(0, -1, 0);
+        scene.AddObject(tetrahedron);
+    }
 
     std::cout << "Added objects to scene.\n";
 
